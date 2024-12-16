ee546342d329824bbc5d22441b091b342154929c
tommichael88 <42842471+tommichael88@users.noreply.github.com>
2024-12-15 21:38:02 -0500

    Update Qqqb.py

--- Qqqb.py
+++ Qqqb.py
@@ -231,7 +231,7 @@ def handle_chat():
 # Serve the HTML file
 @app.route('/')
 def index():
-    return redirect("https://tommichael88.github.io/booktomnyc/qqb.html")
+    return redirect("https://tommichael88.github.io/booktomnyc/Qqqb.html")
 
 if __name__ == '__main__':
     debug_mode = os.environ.get("FLASK_DEBUG", "False").lower() in ["true", "1", "t"]
